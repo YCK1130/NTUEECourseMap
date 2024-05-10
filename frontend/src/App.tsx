97d9b21b21924aa0389499a2ddabe93c921db84e
YCK1130 <ken1130yang@gmail.com>
2024-05-10 21:10:51 +0800

    add route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
-import './styles/component/homepage.css'
-import Homepage from "./pages/Homepage.tsx";
+import "./styles/component/homepage.css"
+import Homepage from "./pages/Homepage.tsx"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
+const MyRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Homepage />} />
+        </Routes>
+    )
+}
 function App() {
-  return (
-      <Homepage/>
-  )
+    return (
+        <Router>
+            <MyRoutes />
+        </Router>
+    )
 }
 
 export default App
